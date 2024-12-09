81d180d7a8261061133d663fbe9228e08e32a10b
Felipe Salustiano Moreira <piposalumoreira@gmail.com>
2024-12-09 19:57:51 -0300

    coletaDados update coleta_dados_web

--- coletaDados/coleta_dados_web.py
+++ coletaDados/coleta_dados_web.py
@@ -1,31 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+# Fazendo a requisição e a extração dos dados 
 url = 'https://wiki.python.org.br/AprendaMais'
 requisicao = requests.get(url)
 extracao = BeautifulSoup(requisicao.text, 'html.parser')
 
-print(extracao.text.strip())
+# print(extracao.text.strip())
 
+# -=-=-= Extraindo só os titulos em <h2> no HTML -=-=-=
 # for linha_texto in extracao.find_all('h2'):
 #     titulo = linha_texto.text.strip()
 #     print('Título: ', titulo)
 
-'''
-Desafio)
-Filtrar tags ['h2', 'p']
-Contar quantos h2 e p existem no documento (linha_texto.name == tag) 
-'''
-cont_h2 = 0
-cont_p = 0
+# -=-=-= Fazendo a contagem de quantas vezes o <h2> e o <p> aparecem no HTML -=-=-=
+# cont_h2 = 0
+# cont_p = 0
 
-for linha_texto in extracao.find_all(['h2', 'p']):
-    if linha_texto.name == 'h2':
-        cont_h2 += 1
-    elif linha_texto.name == 'p':
-        cont_p += 1
+# for linha_texto in extracao.find_all(['h2', 'p']):
+#     if linha_texto.name == 'h2':
+#         cont_h2 += 1
+#     elif linha_texto.name == 'p':
+#         cont_p += 1
 
-print(f'Total <h2>: {cont_h2} -- Total <p>: {cont_p}')
+# print(f'Total de títulos <h2>: {cont_h2}')
+# print(f'Total de parágrafos <p>: {cont_p}')
     
+# -=-=-=- Método simplificado -=-=-=-
+# cont_h2 = len(extracao.find_all('h2'))
+# cont_p = len(extracao.find_all('p'))
+# print(f'Total de títulos <h2>: {cont_h2}')
+# print(f'Total de parágrafos <p>: {cont_p}')
+#     
 
+# -=-=-= Exibindo somente o texto das tags <h2> e <p> -=-=-=
+# for linha_texto in extracao.find_all(['h2', 'p']):
+#     if linha_texto.name == 'h2':
+#         titulo = linha_texto.text.strip()
+#         print('Título: \n', titulo)
+#     elif linha_texto.name == 'p':
+#         titulo = linha_texto.text.strip()
+#         print('Parágrafo: \n', titulo)
 
+# -=-=-= Exibir tags Aninhada -=-=-=
